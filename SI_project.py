f047a1ba95de5aa16d2ad8b467c3ef611ddda746
renardhero <ketteer@gmail.com>
2017-12-18 13:46:15 +0200

    Add files via upload

--- SI_project.py
+++ SI_project.py
@@ -7,6 +7,8 @@ import time
 import os
 import pandas as pd
 import random
+from collections import Counter
+from scipy import stats
 
 
 def SI_model(event_data,p,infected):
@@ -28,7 +30,32 @@ def SI_model(event_data,p,infected):
                     
     return infected 
     
+def SI_model_immunization(event_data,p,infected,immunized):
+    #The for loop loops over events in a dataframe (thus df.iterrows() is used)
+    #The for loop first checks whether the events Source airport is among the infected_airports and that the Destination airport is not amongst them
+    #The for loop then checks that the StartTime of the flight is later than the time that the Source airport got infected
+    #Then random number is generated and checked that it is smaller or equal to the probability of infection and decides whether to infect the destination airport
+    #Then the destination airport is saved amongst the infected airports to the dictionary and to the list
+    
+    infected_airports = list(infected.keys())
+                            
+    for index, flight in event_data.iterrows():
+        if flight["Source"] not in immunized:
+            if flight["Source"] in infected_airports and flight["Destination"] not in infected_airports:
+                if flight["StartTime"] >= infected[flight["Source"]]:
+                    random_n = random.random()
+                    if random_n <= p:
+                        infected[flight["Destination"]] = flight["EndTime"]
+                        infected_airports.append(flight["Destination"])
+                        
+    return infected 
+    
 def prevalence_average(event_data,infected,n,p,bins,iterations,event_range):
+    #Calculates the prevalence of infected node e.g. what percentage of nodes is infected in certain time point
+    #Average is calculcated in each different bin (step of time) over a chosen number of iterations
+    #The resulted prevalence is cumulative
+    #Returns both the bin edges and the list with prevalence percentages for each bin
+     
     count = 0
     prevalence_lists = [[]] * iterations 
     while count < iterations:
@@ -38,8 +65,34 @@ def prevalence_average(event_data,infected,n,p,bins,iterations,event_range):
         prevalence_lists[count] = freq
         count += 1
     prevalence = np.array(prevalence_lists)
-    prevalence = np.mean(prevalence, axis=0)
+    prevalence = np.mean(prevalence, axis=0) #why has the axis=0 be here? 
     return bins[:len(bins)-1], prevalence
+    
+def get_centrality_measures(network):
+    """
+    Calculates five centrality measures (degree, betweenness, closeness, and
+    eigenvector centrality, and k-shell) for the nodes of the given network.
+
+    Parameters
+    ----------
+    network: networkx.Graph()
+    tol: tolerance parameter for calculating eigenvector centrality
+
+    Returns
+    --------
+    [degree, betweenness, closeness, eigenvector_centrality, kshell]: list of
+    numpy.arrays
+    """
+    
+    kshell = nx.core_number(network)
+    clustering = nx.clustering(network)
+    degree = nx.degree_centrality(network)
+    betweenness = nx.betweenness_centrality(network)
+    closeness = nx.closeness_centrality(network)
+    #eigenvector_centrality = nx.eigenvector_centrality(network, tol=tol).values()
+    
+
+    return [kshell, clustering, degree, betweenness, closeness]
     
     
 
@@ -78,21 +131,17 @@ if __name__ == "__main__":
     
     #task1
     
-    #print(SI_model(event_data,p_infection,infected)) #might take some time since event data is so large
-    #print(SI_model(test_events,p_infection,infected))
+    '''print(SI_model(event_data,p_infection,infected)) #might take some time since event data is so large
+    print(SI_model(test_events,p_infection,infected))'''
     
     #task2
-            
-    #print(min(event_data["StartTime"]))
-    #print(max(event_data["EndTime"]))
-    #freq, bins = np.histogram(SI_model(test_events,p_infection,infected).values(), bins=np.linspace(min(event_data["StartTime"]),max(event_data["EndTime"]),10), range=[min(event_data["StartTime"]), max(event_data["EndTime"])], normed=False, weights=None)
-    #freq = [float(x) / n for x in freq]
     
     '''bins = np.linspace(min(event_data["StartTime"]),max(event_data["EndTime"]),num=50) # you can choose the number of bins by changing the scalar
     iterations = 10 # you can choose the number of iterations to get the average from 
     event_range = [min(event_data["StartTime"]),max(event_data["EndTime"])]
+    
     print(p_list[0])
-    values1 = prevalence_average(event_data,infected,n,p_list[3],bins,iterations, event_range)
+    values1 = prevalence_average(event_data,infected,n,p_list[0],bins,iterations, event_range)
     x1 = values1[0]
     y1 = values1[1]
     #values2 = prevalence_average(event_data,infected,n,p_list[1],bins,iterations,event_range)
@@ -122,7 +171,7 @@ if __name__ == "__main__":
     
    #task3
    
-    infected_dict =  {}
+    '''infected_dict =  {}
     n = 279 # number of airports in the network
     p_infection = 0.1 # here you can change the probability of infection
    
@@ -168,49 +217,107 @@ if __name__ == "__main__":
     plt.plot(x4,y4)
     plt.plot(x5,y5)
     plt.legend(['Infected node=0', 'Infected node=4','Infected node=41','Infected node=100','Infected node=200'], loc="best")
-    plt.show()
+    plt.show()'''
     
-        
+    #task4
     
-    '''for index, flight in event_data.iterrows():
-        for key in infected.items():
-             if flight[:2] == key:
-                 infected[flight["Destination"]] = flight["Endtime"]'''
-                    
+    count = 0
+    iterations = 50 # Change the number of iterations
+    start_nodes = []
+    while count < iterations:
+        count += 1
+        start_nodes.append(random.randint(0,278))
+    # Starting nodes are now in storage
     
+
+    p_infection = 0.5
+    count =  0
+    '''si_model_iterations = [[]] * iterations
+    for node in start_nodes:
+        start_node_dict =  {}
+        start_node_dict[node] = 1229231100
+        iteration = SI_model(event_data,p_infection,start_node_dict)
+        si_model_iterations[count] = iteration
+        count += 1
+    # si_model_iterations now includes N=iterations dictionaries and each dictionary holds all infected nodes and infection times
     
-    '''for index, flight in test_events.iterrows():
-        for key in infected.items():
-            if flight["Source"] == key[0]:
-                random_n = random.random()
-                if random_n <= p_infection:
-                    infected[flight["Destination"]] = flight["EndTime"]'''
-        
-    '''infected_airports = list(infected.keys())
-                            
-    for index, flight in event_data.iterrows():
-        if flight["Source"] in infected_airports and flight["Destination"] not in infected_airports:
-            if flight["StartTime"] >= infected[flight["Source"]]:
-                random_n = random.random()
-                if random_n <= p_infection:
-                    infected[flight["Destination"]] = flight["EndTime"]
-                    infected_airports.append(flight["Destination"])'''
-    #The for loop loops over events in a dataframe (thus df.iterrows() is used)
-    #The for loop first checks whether the events Source airport is among the infected_airports and that the Destination airport is not amongst them
-    #The for loop then checks that the StartTime of the flight is later than the time that the Source airport got infected
-    #Then random number is generated and checked that it is smaller or equal to the probability of infection and decides whether to infect the destination airport
-    #Then the destination airport is saved amongst the infected airports to the dictionary and to the list             
+    #let's find out which of the keys have too few occurrences to end up in the final analysis...
+    key_freq_list =  [[]] * iterations 
+    count =  0
+    for i in si_model_iterations:
+        key_freq_list[count] = i.keys()
+        count +=  1
     
-    #print(infected)
-    #df_filtered = test_events[test_events["EndTime"] == 1229269200]
-    #df_filtered2 = test_events[test_events["EndTime"] == 1229266800]
-    #print(df_filtered) 
-    #print(test_events.filter(like=1229269200, axis=3))
+    #making a list of keys where all the occurrences are deleted that are not going to be included in the final analysis
+    limit = 25
+    flat_list = [item for sublist in key_freq_list for item in sublist]
+    key_count =  Counter(flat_list)
+    key_list = key_count.keys()
+    print(key_list)
+    for key in key_count:
+        if key_count[key] < 25:
+            key_list.remove(key)
+    print(key_list)'''
+    
+    new_dict =  {}
+    key_freq = []
+    for node in start_nodes:
+        start_node_dict = {}
+        start_node_dict[node] = 1229231100
+        iteration = SI_model(event_data,p_infection,start_node_dict)
+        for key, value in iteration.items():
+            key_freq.append(key)
+            if key in new_dict:
+                new_dict[key] += value
+            else:
+                new_dict[key] = value
                 
+    key_count =  Counter(key_freq)
+    for key,value in key_count.items():
+        if value < iterations / 2:
+            new_dict.pop('key')
     
-        
-        
-        
-        
-    #dictionary (node ja infection time), jossa kaikki eventit --> verrataan endtimeja noden infection timeen käydään lista läpi (kaikki nodet ei infektoidu)
+    for key, value in new_dict.items():
+        new_dict[key] =  value / key_count[key]
+    
+    #print(new_dict)
+    
+    # Now we have mean infection times for all nodes in new_dict[]
+    # what we have is a dictionary containing all infected nodes and their mean infection times with nodes having less than iteration / 2 infections removed. 
+    
+    #Now we can read in the .edg file so the network that is provided to calculate all network properties for the network
+    #Attention! The network is weighted!
+    #The aggregated weighted network aggregated_US_air_traffic_network_undir.edg is constructed
+    #based on the event data, so that weight of each link corresponds to the number of
+    #flights between the nodes. 
+    
+    network_path = 'C:/Users/kette/Complex networks/aggregated_US_air_traffic_network_undir.edg'
+    network = nx.read_weighted_edgelist(network_path)
+    
+    #print(get_centrality_measures(network)[0].keys())
+    #print(get_centrality_measures(network)[0].values())
+    
+    #converting centrality unicode key values to int
+    centrality_dict = {}
+    centrality = get_centrality_measures(network)[0]
+    for key,value in centrality.items():
+        centrality_dict[int(key)] = value
+    #print(centrality_dict)
+    
+    #deleting keys from centrality dict that are not in new_dict
+    for key in centrality_dict.keys():
+        if key not in new_dict.keys():
+            centrality_dict.pop('key')
+    
+    x = centrality_dict.values()
+    y = new_dict.values()
+    print(stats.spearmanr(centrality_dict.values(),new_dict.values()))
+    
+    
+    #task5
+    
+    p = 0.5
+    iterations = 20
+    
+    #Selection of immuized nodes
     
